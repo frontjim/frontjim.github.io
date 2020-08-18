9fea6ef473b21ed11e8f0109de70173d75bf95f4
frontjim <frontjim@users.noreply.github.com>
2020-08-18 19:00:16 +0300

    Fix bugs

--- jsn.js
+++ jsn.js
@@ -1,17 +1,17 @@
-function op(e) {
-  e.style.color = "ghostwhite";
-  e.childNodes[0].style.left = "0";
-  e.style.fontWeight = "500";
+function o(l) {
+  l.style.color = "ghostwhite";
+  l.childNodes[0].style.left = "0";
+  l.style.fontWeight = "500";
   var arrows = document.getElementsByClassName("iconn");
   for (var i = 0; i < arrows.length; i++) {
-    if (e != arrows[i]) {
+    if (l != arrows[i]) {
       arrows[i].style.color = "transparent";
       arrows[i].childNodes[0].style.left = "20%";
       arrows[i].style.fontWeight = "500";
     }
   }
 }
-function border(q) {
+function borde(q) {
   var arrow = document.getElementsByClassName("iconn");
   for (var i = 0; i < arrow.length; i++) {
     if (q === arrow[i]) {
